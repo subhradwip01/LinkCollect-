ec648471365eb0bc82ceacbc6d0d15b1e0a28eac
anshyyy <anshuman9998@gmail.com>
2023-03-22 13:02:57 +0530

    added database configs

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,10 @@
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 module.exports = {
-    PORT : process.env.PORT
+    PORT : process.env.PORT,
+    DBPASS : process.env.MONGO_PASS,
+    DBUSER : process.env.DB_USER,
+    DBNAME : process.env.DB_NAME
 }
