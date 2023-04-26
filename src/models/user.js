b7b5d417425db673325f6cdb72230770e08e45f5
anshyyy <anshuman9998@gmail.com>
2023-04-26 23:16:04 +0530

    checking a bug

--- src/models/user.js
+++ src/models/user.js
@@ -47,16 +47,16 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.path('collections').validate(function(collections) {
-  return collections.length <= 30; // set your limit here
-}, 'Too many collections',{abortEarly: false});
+// userSchema.path('collections').validate(function(collections) {
+//   return collections.length <= 30; // set your limit here
+// }, 'Too many collections',{abortEarly: false});
 
 userSchema.pre("save", function (next) {
   if (!this.password || !this.isModified("password")||!this.isModified("username")) return next(); // Added for google auth and unnecessary hash changes whenever user.save is called (could have created bugs)
  
-  const validationError = this.validateSync();
-  if (validationError) {
-    return next(validationError);
+  if (this.collections.length > 30) {
+    const err = new Error('Too many collections');
+    return next(err);
   }
   
   const encryptedPassword = bcrypt.hashSync(this.password, Number(SALT));
