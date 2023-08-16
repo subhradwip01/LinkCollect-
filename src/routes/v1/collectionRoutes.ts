84449f48231884a912935d274405fd46e8bffdbc
Harsh8055 <harshsingh.eth@gmail.com>
2023-08-16 18:27:56 +0530

    bug resolved

--- src/routes/v1/collectionRoutes.ts
+++ src/routes/v1/collectionRoutes.ts
@@ -1,11 +1,18 @@
 import express from "express";
 const router = express.Router();
 import collectionController from "../../controllers/collectionController";
-import { isPublicCheck, userExist, checkWhenSomeoneisFetchigCollection } from '../../middlewares/validateRequests';
-import { collectionLimit } from '../../middlewares/limits';
+import {
+  isPublicCheck,
+  userExist,
+  checkWhenSomeoneisFetchigCollection,
+} from "../../middlewares/validateRequests";
+import { collectionLimit } from "../../middlewares/limits";
 import { storage } from "../../cloudinary";
 import isUserPublic from "../../middlewares/isUserPublic";
-import { isCollectionOwner, isCollectionPublic } from "../../middlewares/isCollectionOwner";
+import {
+  isCollectionOwner,
+  isCollectionPublic,
+} from "../../middlewares/isCollectionOwner";
 import multer from "multer";
 const upload = multer({
   storage,
@@ -19,49 +26,65 @@ const upload = multer({
 router.get("/without-timelines", collectionController.getAll);
 
 // Special Route to get all collections that are public by username by ANYONE
-router.get("/user/:username", isUserPublic, collectionController.getAllByUsername)
+router.get(
+  "/user/:username",
+  isUserPublic,
+  collectionController.getAllByUsername
+);
 
 // Check for duplicate link
-router.post("/:id/check-duplicate-link", collectionController.doesLinkExist)
+router.post("/:id/check-duplicate-link", collectionController.doesLinkExist);
 
 // ---------------------------------------------------------------------- //
 
 // CRUD ROUTES
 // get collection from collection id
-router.get("/:id",isCollectionPublic ,collectionController.get);
+router.get("/:id", isCollectionPublic, collectionController.get);
 
 // get all collections with timeline of the user
 router.get("/", collectionController.getAllWithTimeline);
 
 //collection limit removing for testing
-router.post("/" ,upload.single("image"), collectionLimit ,collectionController.create);
-router.patch("/:id", isCollectionOwner, upload.single("image"), collectionController.update);
+router.post(
+  "/",
+  upload.single("image"),
+  collectionLimit,
+  collectionController.create
+);
+router.patch(
+  "/:id",
+  isCollectionOwner,
+  upload.single("image"),
+  collectionController.update
+);
 router.delete("/:id", isCollectionOwner, collectionController.deleteCollection);
-
 
 // save collection routes
 
 // get all saved collection of a user by userid
-router.get("/:id/getsaved", isCollectionOwner, collectionController.getSavedCollections); // userId
+router.get("/:id/getsaved", collectionController.getSavedCollections); // userId
 // save a collection by collection id
-router.post("/:id/save",isCollectionOwner, collectionController.saveCollection);
+router.post("/:id/save", collectionController.saveCollection);
 //unsave a collection by collection id
-router.post("/:id/unsave", isCollectionOwner,collectionController.unsaveCollection);
-
-
-
+router.post("/:id/unsave", collectionController.unsaveCollection);
 
 // UPVOTE ROUTES
 router.post("/:id/upvote", collectionController.upvote);
-router.post('/:id/downvote', collectionController.downvote);
-
+router.post("/:id/downvote", collectionController.downvote);
 
 // Pin Unpin Routes
-router.post("/togglePin/:id", isCollectionOwner, collectionController.togglePin);
+router.post(
+  "/togglePin/:id",
+  isCollectionOwner,
+  collectionController.togglePin
+);
 
 // PRIVACY ROUTES
-router.post('/togglePrivacy/:id', isCollectionOwner, collectionController.togglePrivacy);
-
+router.post(
+  "/togglePrivacy/:id",
+  isCollectionOwner,
+  collectionController.togglePrivacy
+);
 
 // GET VALID TAGS
 router.get("/tags/all", collectionController.getTags); // userId
