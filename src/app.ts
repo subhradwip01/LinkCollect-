338ed1b3800229fc6748d97cb1ef170d16c678a5
Harsh8055 <harshsingh.eth@gmail.com>
2023-09-02 15:35:09 +0530

    rate - 500 per 15 min

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ const setUpAndStartServer = async () => {
 
   // rate limiter 
   const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000, // 15 minutes
     max: 500, // 5Limit each IP to 500 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
